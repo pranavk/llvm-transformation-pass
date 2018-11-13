dd96ea5ec202dca22a4287e508e796449e8a4ae1
John Regehr <regehr@cs.utah.edu>
2018-11-13 12:53:51 -0700

    foo

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -1,5 +1,6 @@
 #include "llvm/Analysis/ValueTracking.h"
 #include "llvm/Analysis/LazyValueInfo.h"
+#include "llvm/Analysis/ScalarEvolution.h"
 #include "llvm/Pass.h"
 #include "llvm/IR/BasicBlock.h"
 #include "llvm/IR/ConstantRange.h"
@@ -20,10 +21,12 @@ struct HelloWorld : public FunctionPass {
 
   void getAnalysisUsage(AnalysisUsage &Info) const {
     Info.addRequired<LazyValueInfoWrapperPass>();
+    Info.addRequired<ScalarEvolutionWrapperPass>();
   }
 
   bool runOnFunction(Function &F) override {
-    LazyValueInfo *LVI = &getAnalysis<LazyValueInfoWrapperPass>().getLVI();
+    auto LVI = &getAnalysis<LazyValueInfoWrapperPass>().getLVI();
+    auto SE = &getAnalysis<ScalarEvolutionWrapperPass>().getSE();
     Module *M = F.getParent();
     errs() << "function: ";
     errs().write_escaped(F.getName()) << '\n';
@@ -31,8 +34,8 @@ struct HelloWorld : public FunctionPass {
       for (auto &I : BB) {
         if (!I.getType()->isIntegerTy())
           continue;
-        errs() << I;
-        errs() << "  ";
+        errs() << I << "\n";
+        errs() << "    ";
         KnownBits Known = computeKnownBits(&I, M->getDataLayout());
         int W = Known.getBitWidth();
         std::string s;
@@ -50,6 +53,11 @@ struct HelloWorld : public FunctionPass {
         }
         errs() << "  ";
         errs() << LVI->getConstantRange(&I, &BB);
+        errs() << "  ";
+        auto SC = SE->getSCEV(&I);
+        SC->print(errs());
+        errs() << "  " << SE->getSignedRange(SC);
+        errs() << "  " << SE->getUnsignedRange(SC);
         errs() << "\n";
       }
     }
@@ -70,5 +78,6 @@ void initializeHelloWorldPass(llvm::PassRegistry &);
 INITIALIZE_PASS_BEGIN(HelloWorld, "print-lazy-value-info",
                       "Lazy Value Info Printer Pass", false, false)
 INITIALIZE_PASS_DEPENDENCY(LazyValueInfoWrapperPass)
+INITIALIZE_PASS_DEPENDENCY(ScalarEvolutionWrapperPass)
 INITIALIZE_PASS_END(HelloWorld, "print-lazy-value-info",
                     "Lazy Value Info Printer Pass", false, false)
