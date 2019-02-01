38379799501babb258bd50cc79a6af5541b78a10
John Regehr <regehr@cs.utah.edu>
2019-02-01 14:48:15 -0700

    pass now removes optnone and noinline attributes from functions

--- RemoveOptnone/RemoveOptnone.cpp
+++ RemoveOptnone/RemoveOptnone.cpp
@@ -16,80 +16,23 @@
 using namespace llvm;
 
 namespace {
-struct HelloWorld : public FunctionPass {
+struct RemoveOptnone : public FunctionPass {
   static char ID;
-  HelloWorld() : FunctionPass(ID) {}
-
-  void getAnalysisUsage(AnalysisUsage &Info) const {
-    Info.addRequired<LazyValueInfoWrapperPass>();
-    Info.addRequired<ScalarEvolutionWrapperPass>();
-    Info.addRequired<DemandedBitsWrapperPass>();
-  }
+  RemoveOptnone() : FunctionPass(ID) {}
 
   bool runOnFunction(Function &F) override {
-    auto LVI = &getAnalysis<LazyValueInfoWrapperPass>().getLVI();
-    if (!LVI)
-      report_fatal_error("LVI initialization failed");
-    auto SE = &getAnalysis<ScalarEvolutionWrapperPass>().getSE();
-    if (!SE)
-      report_fatal_error("SCEV initialization failed");
-    DemandedBits *DB = &getAnalysis<DemandedBitsWrapperPass>().getDemandedBits();
-    if (!DB)
-      report_fatal_error("demanded bits initialization failed");
-    Module *M = F.getParent();
-    errs() << "function: ";
-    errs().write_escaped(F.getName()) << '\n';
-    for (auto &BB : F) {
-      for (auto &I : BB) {
-        if (!I.getType()->isIntegerTy())
-          continue;
-        errs() << I << "\n";
-        errs() << "    ";
-        KnownBits Known = computeKnownBits(&I, M->getDataLayout());
-        int W = Known.getBitWidth();
-        std::string s;
-        APInt Zero = Known.Zero;
-        APInt One = Known.One;
-        for (int x = 0; x < W; ++x) {
-          if (Zero.isSignBitSet())
-            errs() << "0";
-          else if (One.isSignBitSet())
-            errs() << "1";
-          else
-            errs() << "?";
-          Zero <<= 1;
-          One <<= 1;
-        }
-        errs() << "  ";
-        errs() << LVI->getConstantRange(&I, &BB);
-        errs() << "  ";
-        auto SC = SE->getSCEV(&I);
-        SC->print(errs());
-        errs() << "  " << SE->getSignedRange(SC);
-        errs() << "  " << SE->getUnsignedRange(SC);
-        errs() << "\n";
-        auto Demanded = DB->getDemandedBits(&I);
-        errs() << "    demanded: " << Demanded << "\n";
-      }
-    }
-    return false;
+    F.removeFnAttr(Attribute::OptimizeNone);
+    F.removeFnAttr(Attribute::NoInline);
+    return true;
   }
 }; // end of struct Hello
 }  // end of anonymous namespace
 
-char HelloWorld::ID = 0;
-static RegisterPass<HelloWorld> X("helloworld", "Hello World Pass",
-                             false /* Only looks at CFG */,
-                             false /* Analysis Pass */);
+char RemoveOptnone::ID = 0;
+static RegisterPass<RemoveOptnone> X("remove-optnone", "Remove optnone Pass",
+                                     true, true);
 
 namespace llvm {
-void initializeHelloWorldPass(llvm::PassRegistry &);
+void initializeRemoveOptnonePass(llvm::PassRegistry &);
 }
 
-INITIALIZE_PASS_BEGIN(HelloWorld, "hello",
-                      "Lazy Value Info Printer Pass", false, false)
-INITIALIZE_PASS_DEPENDENCY(LazyValueInfoWrapperPass)
-INITIALIZE_PASS_DEPENDENCY(DemandedBitsWrapperPass)
-INITIALIZE_PASS_DEPENDENCY(ScalarEvolutionWrapperPass)
-INITIALIZE_PASS_END(HelloWorld, "hello",
-                    "Lazy Value Info Printer Pass", false, false)
