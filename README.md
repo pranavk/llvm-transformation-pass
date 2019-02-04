ec32f249b320ccf0677b947dba23d2ea95a23fd7
John Regehr <regehr@cs.utah.edu>
2019-02-03 23:20:05 -0700

    Revert "pass now removes optnone and noinline attributes from functions"
    This reverts commit 38379799501babb258bd50cc79a6af5541b78a10.

--- README.md
+++ README.md
@@ -5,33 +5,31 @@
 mkdir build
 cd build
 cmake ..
-make
-cd ..
 ```
 
 ## Test
 
 ```
 clang -O0 -c -emit-llvm test/hello.c
-llvm-dis hello.bc -o -
+llvm-dis test/hello.bc -o -
 ```
 
 (Make sure optnone attributes appear in the output)
 
 ```
-opt -load build/RemoveOptnone/RemoveOptnone.so -RemoveOptnone hello.bc -o hello2.bc
+opt -load build/RemoveOptnone/RemoveOptnone.so -remove-optnone test/hello.bc -o test/hello2.bc
 ```
 
 (On OS X use RemoveOptnone.dylib instead of RemoveOptnone.so)
 
 ```
-llvm-dis hello2.bc -o -
+llvm-dis test/hello2.bc -o -
 ```
 
 (Make sure no optnone attributes appear in the output)
 
 ```
-opt -O1 hello2.bc -S -o -
+opt -O1 test/hello2.bc -S -o -
 ```
 
 (Check that main() returns the constant 0)
--- RemoveOptnone/RemoveOptnone.cpp
+++ RemoveOptnone/RemoveOptnone.cpp
@@ -16,23 +16,80 @@
 using namespace llvm;
 
 namespace {
-struct RemoveOptnone : public FunctionPass {
+struct HelloWorld : public FunctionPass {
   static char ID;
-  RemoveOptnone() : FunctionPass(ID) {}
+  HelloWorld() : FunctionPass(ID) {}
+
+  void getAnalysisUsage(AnalysisUsage &Info) const {
+    Info.addRequired<LazyValueInfoWrapperPass>();
+    Info.addRequired<ScalarEvolutionWrapperPass>();
+    Info.addRequired<DemandedBitsWrapperPass>();
+  }
 
   bool runOnFunction(Function &F) override {
-    F.removeFnAttr(Attribute::OptimizeNone);
-    F.removeFnAttr(Attribute::NoInline);
-    return true;
+    auto LVI = &getAnalysis<LazyValueInfoWrapperPass>().getLVI();
+    if (!LVI)
+      report_fatal_error("LVI initialization failed");
+    auto SE = &getAnalysis<ScalarEvolutionWrapperPass>().getSE();
+    if (!SE)
+      report_fatal_error("SCEV initialization failed");
+    DemandedBits *DB = &getAnalysis<DemandedBitsWrapperPass>().getDemandedBits();
+    if (!DB)
+      report_fatal_error("demanded bits initialization failed");
+    Module *M = F.getParent();
+    errs() << "function: ";
+    errs().write_escaped(F.getName()) << '\n';
+    for (auto &BB : F) {
+      for (auto &I : BB) {
+        if (!I.getType()->isIntegerTy())
+          continue;
+        errs() << I << "\n";
+        errs() << "    ";
+        KnownBits Known = computeKnownBits(&I, M->getDataLayout());
+        int W = Known.getBitWidth();
+        std::string s;
+        APInt Zero = Known.Zero;
+        APInt One = Known.One;
+        for (int x = 0; x < W; ++x) {
+          if (Zero.isSignBitSet())
+            errs() << "0";
+          else if (One.isSignBitSet())
+            errs() << "1";
+          else
+            errs() << "?";
+          Zero <<= 1;
+          One <<= 1;
+        }
+        errs() << "  ";
+        errs() << LVI->getConstantRange(&I, &BB);
+        errs() << "  ";
+        auto SC = SE->getSCEV(&I);
+        SC->print(errs());
+        errs() << "  " << SE->getSignedRange(SC);
+        errs() << "  " << SE->getUnsignedRange(SC);
+        errs() << "\n";
+        auto Demanded = DB->getDemandedBits(&I);
+        errs() << "    demanded: " << Demanded << "\n";
+      }
+    }
+    return false;
   }
 }; // end of struct Hello
 }  // end of anonymous namespace
 
-char RemoveOptnone::ID = 0;
-static RegisterPass<RemoveOptnone> X("remove-optnone", "Remove optnone Pass",
-                                     true, true);
+char HelloWorld::ID = 0;
+static RegisterPass<HelloWorld> X("helloworld", "Hello World Pass",
+                             false /* Only looks at CFG */,
+                             false /* Analysis Pass */);
 
 namespace llvm {
-void initializeRemoveOptnonePass(llvm::PassRegistry &);
+void initializeHelloWorldPass(llvm::PassRegistry &);
 }
 
+INITIALIZE_PASS_BEGIN(HelloWorld, "hello",
+                      "Lazy Value Info Printer Pass", false, false)
+INITIALIZE_PASS_DEPENDENCY(LazyValueInfoWrapperPass)
+INITIALIZE_PASS_DEPENDENCY(DemandedBitsWrapperPass)
+INITIALIZE_PASS_DEPENDENCY(ScalarEvolutionWrapperPass)
+INITIALIZE_PASS_END(HelloWorld, "hello",
+                    "Lazy Value Info Printer Pass", false, false)
