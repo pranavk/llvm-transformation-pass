5ae7f9763f0704cd89fb02a2997aee2df78ee821
Pranav Kant <pranavk@cs.utah.edu>
2019-11-27 22:46:54 -0700

    remove all the lock unlock from the file

--- hello/hello.cpp
+++ hello/hello.cpp
@@ -3,6 +3,13 @@
 #include "llvm/IR/Function.h"
 #include "llvm/IR/Module.h"
 #include "llvm/Support/raw_ostream.h"
+#include "llvm/IR/Instructions.h"
+#include "llvm/PassRegistry.h"
+
+#include "llvm/PassSupport.h"
+#include "llvm/Transforms/IPO/PassManagerBuilder.h"
+#include "llvm/IR/LegacyPassManager.h"
+
 
 #include <string>
 
@@ -16,17 +23,27 @@ struct HelloWorld : public FunctionPass {
 
   bool runOnFunction(Function &F) override {
     Module *M = F.getParent();
-    errs() << "helloworld pass: function: ";
-    errs().write_escaped(F.getName()) << '\n';
+    //errs() << "helloworld pass: function: ";
+    //errs().write_escaped(F.getName()) << '\n';
+    std::vector<Instruction*> vec;
     for (auto &BB : F) {
       for (auto &I : BB) {
 
 	if (auto callInst = dyn_cast<CallInst>(&I)) {
 	  Function* calledFunc = callInst->getCalledFunction();
-	  errs() << "Called function: " << calledFunc->getName() << "\n";
+	  if (calledFunc && (calledFunc->getName() == "mutex_lock" || calledFunc->getName() == "mutex_lock_nested" || calledFunc->getName() == "mutex_unlock")) {
+	    errs() << calledFunc->getName() << "\n";
+	    vec.push_back(&I);
+	    //I.eraseFromParent();
+	  }
 	}
       }
     }
+
+    for (auto I : vec) {
+      I->eraseFromParent();
+    }
+
     return false;
   }
 }; // end of struct Hello
@@ -36,3 +53,16 @@ char HelloWorld::ID = 0;
 static RegisterPass<HelloWorld> X("helloworld", "Hello World Pass",
                              false /* Only looks at CFG */,
                              false /* Analysis Pass */);
+
+static void registerHostBlockProfilerPass(const PassManagerBuilder &,
+                                            legacy::PassManagerBase &PM) {
+  PM.add(new HelloWorld());
+}
+
+// Register Host Pass early so the analysis will work better
+static RegisterStandardPasses
+    RegisterHostPass(PassManagerBuilder::EP_ModuleOptimizerEarly,
+                   registerHostBlockProfilerPass);
+
+static RegisterStandardPasses
+    RegisterHostPass0(PassManagerBuilder::EP_EnabledOnOptLevel0, registerHostBlockProfilerPass);
